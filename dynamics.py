7ee60a38a94bbaa9f7643fd5abde7bcc51b0821c
Cornelius Wolff <wolffcornelius@gmx.de>
2023-07-30 22:43:20 +0200

    feat: Added experiments for the first paper

---
+++ dynamics.py
@@ -0,0 +1,152 @@
+from MuJoCo_Gym.mujoco_rl import MuJoCoRL
+import numpy as np
+import cv2
+import copy
+from sklearn.metrics import mean_squared_error
+from autoencoder import Autoencoder
+
+class Image:
+    def __init__(self, environment):
+        self.environment = environment
+        self.observation_space = {"low": [0 for _ in range(30)], "high": [6000 for _ in range(30)]}
+        self.action_space = {"low": [], "high": []}
+        self.autoencoder = Autoencoder(latent_dim=30, input_shape=(64, 64, 3))
+        self.autoencoder.encoder.load_weights("models/encoder30.h5")
+        self.index = 0
+
+    def dynamic(self, agent, actions):
+        self.index = self.index + 1
+        image = self.environment.get_camera_data(agent + "_camera")
+        image = cv2.resize(image, (64, 64))
+        result = self.autoencoder.encoder.predict(np.array([image]), verbose=0)[0]
+        # cv2.imwrite("EmbodiedReference/images/" + str(self.index) + ".png", image)
+        return 0, result
+    
+class Communication:
+    def __init__(self, environment):
+        self.environment = environment
+        self.observation_space = {"low": [0, 0, 0, 0], "high": [1, 1, 1, 1]}
+        self.action_space = {"low": [0, 0, 0, 0], "high": [1, 1, 1, 1]}
+
+    def dynamic(self, agent, actions):
+        if "utterance" not in self.environment.data_store[agent].keys():
+            self.environment.data_store[agent]["utterance"] = None
+        if agent == "receiver":
+            utterance = [0, 0, 0, 0]
+            if "target_color" in self.environment.data_store.keys():
+                utterance[np.argmax(self.environment.data_store["target_color"])] = 1
+            observation = utterance
+        elif agent == "sender":
+            utterance = [0, 0, 0, 0]
+            utterance[np.argmax(actions)] = 1
+            self.environment.data_store[agent]["utterance"] = actions
+            self.environment.data_store[agent]["utterance_max"] = utterance
+            observation = [0, 0, 0, 0]
+        else:
+            print("Dafaq is going on here?")
+        return 0, observation
+    
+class Accuracy:
+    def __init__(self, environment):
+        self.environment = environment
+        self.observation_space = {"low": [], "high": []}
+        self.action_space = {"low": [], "high": []}
+        self.accuracies = []
+        self.variances = []
+        self.sendAccuracies = []
+        self.sendVariances = []
+        self.currentSend = []
+
+    def dynamic(self, agent, actions):
+        choices = ["choice_1", "choice_2"]
+        variance = {"choice_1":1, "choice_2":-1}
+        if "target" in self.environment.data_store.keys():
+            if "sendVariances" not in self.environment.data_store.keys():
+                self.environment.data_store["sendVariances"] = True
+                self.currentSend = [0, 0, 0, 0]
+            target = self.environment.data_store["target"]
+            if self.environment.collision("receiver_geom", target + "_geom"):
+                self.accuracies.append(1)
+                self.variances.append(variance[target])
+            elif self.environment.collision("receiver_geom", [choice for choice in choices if choice != target][0] + "_geom"):
+                self.accuracies.append(0)
+                self.variances.append(variance[[choice for choice in choices if choice != target][0]])
+            reference = [0, 0, 0, 0]
+            color = self.environment.data_store["target_color"]
+            reference[np.argmax(color)] = 1
+            self.currentSend = np.add(self.currentSend, self.environment.data_store[agent]["utterance_max"])
+
+            if self.environment.data_store[agent]["utterance_max"]  == reference:
+                self.sendAccuracies.append(1)
+            else:
+                self.sendAccuracies.append(0)
+        return 0, []
+    
+class Reward:
+    def __init__(self, environment):
+        self.environment = environment
+        self.observation_space = {"low": [], "high": []}
+        self.action_space = {"low": [], "high": []}
+        self.choices = ["choice_1", "choice_2"]
+
+    def dynamic(self, agent, actions):
+        if not "target" in self.environment.data_store.keys():
+            color = self.environment.get_data("reference_geom")["color"]
+            for choice in self.choices:
+                if (color == self.environment.get_data(choice + "_geom")["color"]).all():
+                    self.environment.data_store["target"] = choice
+                    self.environment.data_store["target_color"] = self.environment.get_data(choice + "_geom")["color"]
+                    self.environment.data_store["last_distance"] = copy.deepcopy(self.environment.distance("receiver_geom", choice + "_geom"))
+        if agent == "receiver":
+            target = self.environment.data_store["target"]
+            new_distance = self.environment.distance("receiver_geom", target + "_geom")
+            reward = self.environment.data_store["last_distance"] - new_distance
+            self.environment.data_store["last_distance"] = copy.deepcopy(new_distance)
+        elif agent == "sender":
+            reference = [0, 0, 0, 0]
+            color = self.environment.data_store["target_color"]
+            reference[np.argmax(color)] = 1
+            reward = 0
+            if "utterance" in self.environment.data_store[agent].keys():
+                reward = -1 * mean_squared_error(reference, self.environment.data_store[agent]["utterance"])
+                # if reference == self.environment.data_store[agent]["utterance_max"]:
+                #     reward = 0.01
+                # else:
+                #     reward = -0.01
+        return reward, []
+
+    
+def target_reward(mujoco_gym, agent):
+    choices = ["choice_1", "choice_2"]
+    if not "target" in mujoco_gym.data_store.keys():
+            color = mujoco_gym.get_data("reference_geom")["color"]
+            for choice in choices:
+                if (color == mujoco_gym.get_data(choice + "_geom")["color"]).all():
+                    mujoco_gym.data_store["target"] = choice
+                    mujoco_gym.data_store["target_color"] = mujoco_gym.get_data(choice + "_geom")["color"]
+    
+    target = mujoco_gym.data_store["target"]
+    reward = 0
+    if mujoco_gym.collision("receiver_geom", target + "_geom"):
+        return 1
+    elif mujoco_gym.collision("receiver_geom", [choice for choice in choices if choice != target][0] + "_geom"):
+        return -1
+    return reward
+
+def collision_reward(mujoco_gym, agent):
+    for border in ["border1_geom", "border2_geom", "border3_geom", "border4_geom", "border5_geom"]:
+        if mujoco_gym.collision(border, agent + "_geom"):
+            return -0.1
+    return 0
+        
+def target_done(mujoco_gym, agent):
+    for choice in ["choice_1", "choice_2"]:
+        if(mujoco_gym.collision(choice + "_geom", agent + "_geom")):
+            return True
+    return False
+
+def border_done(mujoco_gym, agent):
+    for border in ["border1_geom", "border2_geom", "border3_geom", "border4_geom", "border5_geom"]:
+        if mujoco_gym.collision(border, agent + "_geom"):
+            return True
+    return False
---
+++ language_training.py
@@ -0,0 +1,72 @@
+import time
+from dynamics import *
+from MuJoCo_Gym.mujoco_rl import MuJoCoRL
+from MuJoCo_Gym.wrappers import GymnasiumWrapper
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+import os
+from gymnasium.wrappers.frame_stack import FrameStack
+from gymnasium.experimental.wrappers import NormalizeObservationV0
+
+class TensorboardCallback(BaseCallback):
+    """
+    Custom callback for plotting additional values in tensorboard.
+    """
+    def __init__(self, verbose=0):
+        super().__init__(verbose)
+    def _on_step(self) -> bool:
+        # Log scalar value (here a random variable)
+        for env in self.training_env.envs:
+            if isinstance(env.env.env.environment.environment_dynamics[2], Accuracy):
+                accuacies = env.env.env.environment.environment_dynamics[2].accuracies
+                variances = env.env.env.environment.environment_dynamics[2].variances
+                sendAccuracies = env.env.env.environment.environment_dynamics[2].sendAccuracies
+                if len(accuacies) > 50:
+                    value = sum(accuacies[-50:]) / 50
+                    self.logger.record("accuracy", value)
+
+                    variance = 1 - abs(sum(variances[-50:]) / 50)
+                    self.logger.record("variance", variance)
+                if len(sendAccuracies) > 16000:
+                    value = sum(sendAccuracies[-16000:]) / 16000
+                    self.logger.record("send accuracy", value)
+        return True
+    
+
+xml_files = ["levels/" + file for file in os.listdir("levels/")]
+window = 5
+learning_rate = 1e-6
+network = [256, 128]
+batch_size = 32
+device = "cuda"
+timesteps = 250000
+
+
+
+agents = ["sender"]
+config_dict = {"xmlPath":xml_files, "agents":agents, "rewardFunctions":[collision_reward, target_reward], "doneFunctions":[target_done, border_done], "skipFrames":60, "environmentDynamics":[Image, Communication, Accuracy, Reward], "freeJoint":True, "renderMode":False, "maxSteps":512, "agentCameras":True}
+policy_kwargs = dict(
+                net_arch=dict(pi=network, vf=network),
+)
+env = MuJoCoRL(config_dict=config_dict)
+env = GymnasiumWrapper(env, "sender")
+env = NormalizeObservationV0(FrameStack(env, window))
+name = "PPO Sender"
+model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, batch_size=batch_size, device=device, tensorboard_log="./results/", learning_rate=learning_rate, stats_window_size=200)
+model.learn(timesteps, tb_log_name=name, progress_bar=True, callback=TensorboardCallback())
+model.save("models/Sender" + str(int(time.time())))
+
+
+
+agents = ["receiver"]
+config_dict = {"xmlPath":xml_files, "agents":agents, "rewardFunctions":[collision_reward, target_reward], "doneFunctions":[target_done, border_done], "skipFrames":60, "environmentDynamics":[Image, Communication, Accuracy, Reward], "freeJoint":True, "renderMode":False, "maxSteps":512, "agentCameras":True}
+policy_kwargs = dict(
+                net_arch=dict(pi=network, vf=network),
+)
+env = MuJoCoRL(config_dict=config_dict)
+env = GymnasiumWrapper(env, "receiver")
+env = NormalizeObservationV0(FrameStack(env, window))
+name = "PPO Receiver"
+model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, batch_size=batch_size, device=device, tensorboard_log="./results/", learning_rate=learning_rate, stats_window_size=200)
+model.learn(timesteps, tb_log_name=name, progress_bar=True, callback=TensorboardCallback())
+model.save("models/Receiver" + str(int(time.time())))
